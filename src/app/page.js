2a1e514f6c269857e243e405dd801bc5bd932f71
jahidulkanchan <mdj524156@gmail.com>
2025-03-12 00:24:34 +0600

    Readme update

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center'>
       <h1 className='text-4xl text-center text-green'>Welcome to our Edu Genius Website</h1>
-      <p className='text-2xl md:text-3xl'>AI-Powered Course Management System</p>
+      <p className='text-2xl'>AI-Powered Course Management System</p>
     </div>
   )
 }
