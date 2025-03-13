af8410ed880540190083487310895f250e568eaa
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-03-14 02:44:30 +0600

    Add course management actions and establish database connection

---
+++ src/lib/actions/course.action.js
@@ -0,0 +1,35 @@
+"use server";
+
+import Course from "@/models/Course";
+import dbConnect from "../dbConnect";
+
+export async function createCourse(courseData) {
+  try {
+    await dbConnect();
+    const newCourse = new Course(courseData);
+    return await newCourse.save();
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to create course");
+  }
+}
+
+export async function updateCourse(courseId, courseData) {
+  try {
+    await dbConnect();
+    return await Course.findByIdAndUpdate(courseId, courseData, { new: true });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to update course");
+  }
+}
+
+export async function deleteCourse(courseId) {
+  try {
+    await dbConnect();
+    return await Course.findByIdAndDelete(courseId);
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to delete course");
+  }
+}
