8d090e5d187c4763d158b04079ef62f38ab24ac3
md.munna <mk8761174@gmail.com>
2025-03-13 23:10:23 +0600

    add some section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import TotalCourse from './components/TotalCourse'
-import CourseSubjects from './components/CourseSubjects'
-import TopInstructors from './components/TopInstructors'
-import BecomeInstructor from './components/BecomeInstructor'
-import OurAchieve from './components/OurAchieve'
-import Testimonial from './components/Testimonial'
+import React from "react";
+
+import BecomeInstructor from "@/components/home/BecomeInstructor";
+import CourseSubjects from "@/components/home/CourseSubjects";
+import TopInstructors from "@/components/home/TopInstructors";
+import TotalCourse from "@/components/home/TotalCourse";
+import OurAchieve from "@/components/shared/OurAchieve";
+import Testimonial from "@/components/shared/Testimonial";
 
 export default function Home() {
   return (
-    <div className=''>
-     
+    <div className="">
       <TopInstructors />
 
-<BecomeInstructor />
-      <TotalCourse></TotalCourse>
-      <CourseSubjects></CourseSubjects>
-      <OurAchieve></OurAchieve>
-      <Testimonial></Testimonial>
+      <BecomeInstructor />
+      <TotalCourse/>
+      <CourseSubjects/>
+      <OurAchieve/>
+      <Testimonial/>
     </div>
-  )
+  );
 }
