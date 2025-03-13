287e2fa36c6a0c9252594b6eedd45b1f46f576e7
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-03-13 21:36:32 +0600

    Add user and course models with enhanced schema definitions

--- src/models/User.js
+++ src/models/User.js
@@ -1,19 +1,20 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    clerkUserId: { type: String, required: true, unique: true }, // Clerk user ID
+    email: { type: String, required: true, unique: true }, // Clerk email
+    firstName: { type: String }, // Clerk first name
+    lastName: { type: String }, // Clerk last name
+    role: {
+      type: String,
+      enum: ["student", "instructor", "admin"],
+      default: "student",
+      required: true,
+    }, // User role
+    profilePicture: { type: String }, // Clerk profile picture URL
   },
-  name: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
-export default mongoose.models.User || mongoose.model("User", UserSchema);
+export default mongoose.models.User || mongoose.model("User", userSchema);
