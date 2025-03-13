03a88bc173a3d40a1a5407159310412bea030772
md.munna <mk8761174@gmail.com>
2025-03-13 22:50:16 +0600

    add some feature

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,8 @@ import TotalCourse from './components/TotalCourse'
 import CourseSubjects from './components/CourseSubjects'
 import TopInstructors from './components/TopInstructors'
 import BecomeInstructor from './components/BecomeInstructor'
+import OurAchieve from './components/OurAchieve'
+import Testimonial from './components/Testimonial'
 
 export default function Home() {
   return (
@@ -13,6 +15,8 @@ export default function Home() {
 <BecomeInstructor />
       <TotalCourse></TotalCourse>
       <CourseSubjects></CourseSubjects>
+      <OurAchieve></OurAchieve>
+      <Testimonial></Testimonial>
     </div>
   )
 }
