02ec3686e812dfbc5661203cd5ca3809e5e70e14
tariqul420 <tariqul.developer@gmail.com>
2025-03-13 09:15:48 +0600

    Refactor BecomeInstructor and Footer components for improved layout and responsiveness

--- src/app/components/BecomeInstructor.jsx
+++ src/app/components/BecomeInstructor.jsx
@@ -2,25 +2,27 @@ import Image from "next/image";
 
 const BecomeInstructor = () => {
   return (
-    <div className="flex items-center justify-between bg-[#f8f8f8] px-40 py-12">
-      <div>
-        <h1 className="font-bold text-2xl">Become an Instructor</h1>
-        <p className="text-gray-500 mb-6 text-sm">
-          Here you can become an Instructor
-        </p>
-        <button className="cursor-pointer px-6 py-3 bg-green text-white font-semibold hover:bg-zinc-700">
-          Become Instructor
-        </button>
+    <section className="bg-[#f8f8f8]  py-12 ">
+      <div className="flex items-center justify-between container mx-auto lg:max-w-6xl">
+        <div>
+          <h1 className="font-bold text-2xl">Become an Instructor</h1>
+          <p className="text-gray-500 mb-6 text-sm">
+            Here you can become an Instructor
+          </p>
+          <button className="cursor-pointer px-6 py-3 bg-green text-white font-semibold hover:bg-zinc-700">
+            Become Instructor
+          </button>
+        </div>
+        <div>
+          <Image
+            src="/images/become_instructor.png"
+            width={500}
+            height={500}
+            alt="become an instructor"
+          />
+        </div>
       </div>
-      <div>
-        <Image
-          src="/images/become_instructor.png"
-          width={500}
-          height={500}
-          alt="become an instructor"
-        />
-      </div>
-    </div>
+    </section>
   );
 };
 
