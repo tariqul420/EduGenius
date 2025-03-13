f5646fa0b163681942d502fbe30374d763be398c
Your Name <golamsarwar1896@gmail.com>
2025-03-13 14:39:05 +0600

    fixed responsiveness of the sections

--- src/app/components/BecomeInstructor.jsx
+++ src/app/components/BecomeInstructor.jsx
@@ -2,16 +2,20 @@ import Image from "next/image";
 
 const BecomeInstructor = () => {
   return (
-    <section className="bg-[#f8f8f8]  py-12 ">
-      <div className="flex items-center justify-between container mx-auto lg:max-w-6xl">
+    <section className="bg-[#f8f8f8] py-12 px-10">
+      <div className="flex flex-col md:flex-row items-center justify-between container mx-auto lg:max-w-6xl">
         <div>
-          <h1 className="font-bold text-2xl">Become an Instructor</h1>
-          <p className="text-gray-500 mb-6 text-sm">
+          <h1 className="font-bold lg:text-2xl text-xl lg:text-left text-center">
+            Become an Instructor
+          </h1>
+          <p className="text-gray-500 lg:mb-6 mb-3 text-sm lg:text-left text-center">
             Here you can become an Instructor
           </p>
-          <button className="cursor-pointer px-6 py-3 bg-green text-white font-semibold hover:bg-zinc-700">
-            Become Instructor
-          </button>
+          <div className="flex justify-center items-center">
+            <button className="cursor-pointer px-6 py-3 bg-green text-white font-semibold hover:bg-zinc-700">
+              Become Instructor
+            </button>
+          </div>
         </div>
         <div>
           <Image
