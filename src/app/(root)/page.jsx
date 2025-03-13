f8a382184789af4dc13a0c73ac458db5ef540b6b
tariqul420 <tariqul.developer@gmail.com>
2025-03-13 23:41:32 +0600

    Refactor Home component layout and remove deprecated page.jsx file

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -3,15 +3,19 @@ import BecomeInstructor from "../../components/home/BecomeInstructor";
 import TopInstructors from "../../components/home/TopInstructors";
 import TotalCourse from "@/components/home/TotalCourse";
 import CourseSubjects from "@/components/home/CourseSubjects";
+import OurAchieve from "@/components/shared/OurAchieve";
+import Testimonial from "@/components/shared/Testimonial";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <HeroSection />
-      <TopInstructors />
-      <BecomeInstructor />
       <TotalCourse />
       <CourseSubjects />
+      <TopInstructors />
+      <OurAchieve />
+      <BecomeInstructor />
+      <Testimonial />
     </div>
   );
 }
