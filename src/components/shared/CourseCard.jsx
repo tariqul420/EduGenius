f625c8d1672a5f9c4dd754189ad102b52eb111c4
tariqul420 <tariqul.developer@gmail.com>
2025-03-16 22:30:21 +0600

    update improve layout and styling of course components for better responsiveness and visual consistency

--- src/components/shared/CourseCard.jsx
+++ src/components/shared/CourseCard.jsx
@@ -16,10 +16,10 @@ import React from "react";
 // },
 
 const CourseCard = ({ course }) => {
-  const {id,rating,category} = course;
+  const { id, rating, category } = course;
   return (
     <>
-      <div className="course-image relative">
+      <div className="course-image relative dark:bg-black-light">
         <p className="bg-green w-fit text-white absolute px-3 py-1.5 text-sm">{category}</p>
         <Image
           src={course.image}
@@ -28,7 +28,7 @@ const CourseCard = ({ course }) => {
           height="100"
           className="w-full h-48 max-w-[300px] mx-auto object-cover rounded-t"
         />
-        <p className="bg-black rounded w-fit flex items-center gap-1.5 text-white absolute right-8 -bottom-2 px-3 py-1 text-sm"><Star fill="yellow" size={16} className="text-orange-400"/> {rating}</p>
+        <p className="bg-black rounded w-fit flex items-center gap-1.5 text-white absolute right-8 -bottom-2 px-3 py-1 text-sm"><Star fill="yellow" size={16} className="text-orange-400" /> {rating}</p>
       </div>
       <div className="course-content p-3">
         <h3 className="text-lg font-semibold">{course.name}</h3>
@@ -38,8 +38,8 @@ const CourseCard = ({ course }) => {
           <div className="enroll flex gap-1.5 items-center"><Users size={16} /> 2 Enrolled</div>
         </div>
         <div className="flex justify-between">
-            <p className="text-green font-medium text-xl md:text-2xl">$23</p>
-            <Link className="bg-green text-white text-sm py-1.5 px-5 rounded" href={`/courses/${id}`}>Details</Link>
+          <p className="text-green font-medium text-xl md:text-2xl">$23</p>
+          <Link className="bg-green text-white text-sm py-1.5 px-5 rounded" href={`/courses/${id}`}>Details</Link>
         </div>
       </div>
     </>
