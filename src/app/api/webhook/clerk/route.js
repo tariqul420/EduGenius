9a8b58d21a8218cd3d5a8364afa026aacb3928c9
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-03-16 11:03:41 +0600

    add Prettier configuration, update user actions for better error handling, and modify ngrok settings

--- src/app/api/webhook/clerk/route.js
+++ src/app/api/webhook/clerk/route.js
@@ -5,38 +5,32 @@ import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 
 export async function POST(req) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const SIGNING_SECRET = process.env.WEBHOOK_SECRET;
 
-  if (!WEBHOOK_SECRET) {
+  if (!SIGNING_SECRET) {
     throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local",
     );
   }
 
-  // Get the headers
-  const headerPayload = headers();
+  const headerPayload = await headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
 
-  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
+    return new Response("Error: Missing Svix headers", {
       status: 400,
     });
   }
 
-  // Get the body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
-  // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
+  const wh = new Webhook(SIGNING_SECRET);
 
   let evt;
 
-  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -44,38 +38,41 @@ export async function POST(req) {
       "svix-signature": svix_signature,
     });
   } catch (err) {
-    // console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
+    console.error("Error: Could not verify webhook:", err);
+    return new Response("Error: Verification error", {
       status: 400,
     });
   }
 
-  // Get the ID and type
-  const { id } = evt.data;
   const eventType = evt.type;
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name } = evt.data;
 
     const user = {
-      clerkId: id,
+      clerkUserId: id,
       email: email_addresses[0].email_address,
       firstName: first_name,
       lastName: last_name,
-      photo: image_url,
+      profilePicture: image_url,
     };
 
-    const newUser = await createUser(user);
+    try {
+      const newUser = await createUser(user);
 
-    if (newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
-        publicMetadata: {
-          userId: newUser.id,
-        },
-      });
+      if (newUser) {
+        await clerkClient.users.updateUserMetadata(id, {
+          publicMetadata: {
+            userId: newUser.id,
+          },
+        });
+      }
+
+      return NextResponse.json({ message: "OK", user: newUser });
+    } catch (error) {
+      console.error("Error creating user:", error);
+      return new Response("Error creating user", { status: 500 });
     }
-
-    return NextResponse.json({ message: "OK", user: newUser });
   }
 
   if (eventType === "user.updated") {
@@ -87,19 +84,25 @@ export async function POST(req) {
       photo: image_url,
     };
 
-    // console.log(user);
-
-    const updatedUser = await updateUser(id, user);
-
-    return NextResponse.json({ message: "OK", user: updatedUser });
+    try {
+      const updatedUser = await updateUser(id, user);
+      return NextResponse.json({ message: "OK", user: updatedUser });
+    } catch (error) {
+      console.error("Error updating user:", error);
+      return new Response("Error updating user", { status: 500 });
+    }
   }
 
   if (eventType === "user.deleted") {
     const { id } = evt.data;
 
-    const deletedUser = await deleteUser(id);
-
-    return NextResponse.json({ message: "OK", user: deletedUser });
+    try {
+      const deletedUser = await deleteUser(id);
+      return NextResponse.json({ message: "OK", user: deletedUser });
+    } catch (error) {
+      console.error("Error deleting user:", error);
+      return new Response("Error deleting user", { status: 500 });
+    }
   }
 
   return new Response("", { status: 200 });
--- src/lib/actions/user.actions.js
+++ src/lib/actions/user.actions.js
@@ -9,43 +9,33 @@ export async function createUser(user) {
     const newUser = new User(user);
     return await newUser.save();
   } catch (error) {
-    // console.log(error);
+    console.error("Error creating user:", error);
   }
 }
 
 export async function updateUser(clerkId, user) {
   try {
     await dbConnect();
-    return await User.findOneAndUpdate({ clerkId }, user, { new: true });
+    return await User.findOneAndUpdate({ clerkUserId: clerkId }, user, {
+      new: true,
+    });
   } catch (error) {
-    // console.log(error);
+    console.error("Error updating user:", error);
   }
 }
 
 export async function deleteUser(clerkId) {
   try {
     await dbConnect();
-    // Find user to delete
-    const user = await User.findOne({ clerkId });
+    const user = await User.findOne({ clerkUserId: clerkId });
 
     if (!user) {
-      throw new Error("User not found");
+      console.error("Error deleting user:", error);
     }
 
-    // Unlink relationships
-    // await Category.deleteMany({ userId: user._id });
-    // await Transaction.deleteMany({ userId: user._id });
     await User.deleteOne({ _id: user._id });
+    return user;
   } catch (error) {
-    // console.log(error);
+    console.error("Error deleting user:", error);
   }
 }
-
-// export async function getUserByClerkId() {
-//   try {
-//     await dbConnect();
-//     return await User.find({});
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
