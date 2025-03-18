d8686035ed4cd154e12d3a1bed7388ae684ef6d6
tariqul420 <tariqul.developer@gmail.com>
2025-03-18 15:23:02 +0600

    update TopInstructors component to enhance heading structure and improve UI layout

--- src/components/home/TopInstructors.jsx
+++ src/components/home/TopInstructors.jsx
@@ -5,9 +5,14 @@ import SlidePrViewSlider from "../shared/SlidePerViewSlider";
 export default function TopInstructors() {
   return (
     <section>
-      <div className="bg-[#08261c] py-8">
+      <div className="bg-[#08261c] py-8 mt-20">
         <div className="container mx-auto py-8 max-lg:px-4 lg:max-w-6xl">
-          <Heading title={`Top Rated Instructor`} subTitle={`The cost of receiving higher education in the United States has skyrocketed to impossible.`} />
+          {/* Heading */}
+          <div className="text-center mb-8">
+            <h1 className="text-3xl font-bold text-white">Top Rated Instructor</h1>
+            <p className="mt-2 text-lg text-gray-400 dark:text-gray-200">The cost of receiving higher education in the United States has skyrocketed to impossible.</p>
+          </div>
+
           <div>
             <SlidePrViewSlider />
           </div>
