edb9e1a08b92a0bfd074237c4f1576c4f5d20699
tariqul420 <tariqul.developer@gmail.com>
2025-03-20 23:54:30 +0600

    remove unused error, layout, and loading components; add category action for fetching categories

--- src/app/(dashboard)/dashboard/(student)/layout.jsx
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <div className="flex min-h-screen w-full items-center justify-center">
-      {children}
-    </div>
-  );
-}
--- src/app/(dashboard)/layout.jsx
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <div className="container mx-auto lg:max-w-6xl">
-      {children}
-    </div>
-  );
-}
