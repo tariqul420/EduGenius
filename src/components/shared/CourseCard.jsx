d9caa6dbd7b5c874c51e273cc32c077c8a2c8fbc
jahidulkanchan <mdj524156@gmail.com>
2025-03-20 10:26:50 +0600

    card design fix

--- src/components/shared/CourseCard.jsx
+++ src/components/shared/CourseCard.jsx
@@ -3,14 +3,22 @@ import Image from "next/image";
 import Link from "next/link";
 
 const CourseCard = ({ course }) => {
-
-  const { slug, category, price, language, level, thumbnail, title, averageRating } = course || {};
+  const {
+    _id,
+    category,
+    price,
+    language,
+    level,
+    thumbnail,
+    title,
+    averageRating,
+  } = course || {};
 
   return (
     <>
-       <div className="course-image relative">
+      <div className="course-image relative">
         {/* Category Badge */}
-        <p className="bg-green absolute top-0 left-0 w-fit px-3 py-1.5 text-sm text-white rounded z-[1]">
+        <p className="bg-green absolute top-0 left-0 z-[1] w-fit rounded px-3 py-1.5 text-sm text-white">
           {category}
         </p>
 
@@ -19,7 +27,7 @@ const CourseCard = ({ course }) => {
           <Image
             src={thumbnail}
             alt={title}
-            className="w-[300px] mx-auto"
+            className="mx-auto w-[300px]"
             placeholder="blur"
             blurDataURL={thumbnail}
             width={100}
@@ -32,7 +40,8 @@ const CourseCard = ({ course }) => {
           className="absolute right-2 -bottom-2 flex w-fit items-center gap-1.5 rounded bg-black px-3 py-1 text-sm text-white"
           aria-label={`Rating: 4.5 stars`}
         >
-          <Star fill="yellow" size={16} className="text-orange-400" /> {averageRating}
+          <Star fill="yellow" size={16} className="text-orange-400" />{" "}
+          {averageRating}
         </p>
       </div>
 
@@ -52,10 +61,12 @@ const CourseCard = ({ course }) => {
 
         {/* Price and Details Button */}
         <div className="flex justify-between">
-          <p className="text-green text-xl font-medium md:text-2xl">$ {price}</p>
+          <p className="text-green text-xl font-medium md:text-2xl">
+            $ {price}
+          </p>
           <Link
-            className="bg-green rounded px-5 py-1.5 text-sm text-white hover:bg-green-600 transition-colors"
-            href={`/courses/${slug}`}
+            className="bg-green rounded px-5 py-1.5 text-sm text-white transition-colors hover:bg-green-600"
+            href={`/courses/${_id}`}
             aria-label={`View details for ${title}`}
           >
             Details
