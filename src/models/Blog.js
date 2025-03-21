363cbf622c6ff44a762683d2675d94648b0012d9
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-03-21 09:40:35 +0600

    add Blog and Comment models with slug generation and timestamps

---
+++ src/models/Blog.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import slugify from "slugify";
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 200,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    thumbnail: String,
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+  },
+  { timestamps: true },
+);
+
+// Pre-save middleware to generate a unique slug from the title
+blogSchema.pre("save", async function (next) {
+  if (this.isModified("title")) {
+    let slug = slugify(this.title, { lower: true, strict: true });
+    let uniqueSlug = slug;
+
+    // Check if the slug already exists in the database
+    const existingBlog = await mongoose.models.Blog.findOne({
+      slug: uniqueSlug,
+    });
+    let counter = 1;
+
+    // If the slug exists, append a unique suffix
+    while (existingBlog) {
+      uniqueSlug = `${slug}-${counter}`;
+      counter++;
+      const duplicateCheck = await mongoose.models.Blog.findOne({
+        slug: uniqueSlug,
+      });
+      if (!duplicateCheck) break;
+    }
+
+    this.slug = uniqueSlug;
+  }
+  next();
+});
+
+export default mongoose.models.Blog || mongoose.model("Blog", blogSchema);
