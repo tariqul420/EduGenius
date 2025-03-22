a60b478cfc2753af476737c53d30f23a00c3de80
tariqul420 <tariqul.developer@gmail.com>
2025-03-22 17:03:17 +0600

    remove unused blog components and implement blog details page with slug routing

--- src/app/(root)/blog/[slug]/page.jsx
+++ src/app/(root)/blog/[slug]/page.jsx
@@ -1,17 +1,12 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import { getBlogBySlug } from "@/lib/actions/blog.action";
+import { CalendarDays, User } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useParams } from "next/navigation";
-import { CalendarDays, User } from "lucide-react";
-import LoadMore from "@/components/shared/LoadMore";
-import Like from "../DetailsLike";
-import BlogDetails from "../page";
-import DetailsLike from "../DetailsLike";
 
+const CourseDetails = async ({ params }) => {
+  const { slug } = await params;
+  const blog = await getBlogBySlug(slug);
 
-
-const CourseDetails = () => {
   const course = {
     id: 1,
     name: "This is Demo Details page",
@@ -25,15 +20,15 @@ const CourseDetails = () => {
 
   return (
     <div className="grid grid-cols-7  container mx-auto px-4 py-6 lg:max-w-6xl my-10   ">
-      
+
       <div className=" md:col-span-5 col-span-7  px-4 md:px-8 mx-auto  items-center rounded-lg p-6  max-w-3xl">
-     <div  className="flex space-x-4 mb-4">
-     <CalendarDays />
-     <p className="text-sm flex text-gray-500">
-           {course.date} |<User></User>  By admin
-        </p> 
-        <hr className="mb-8 border-t-2 border-gray-500" />
-     </div>
+        <div className="flex space-x-4 mb-4">
+          <CalendarDays />
+          <p className="text-sm flex text-gray-500">
+            {course.date} |<User></User>  By admin
+          </p>
+          <hr className="mb-8 border-t-2 border-gray-500" />
+        </div>
         <Image
           src={course.image}
           alt={course.name}
@@ -47,7 +42,7 @@ const CourseDetails = () => {
         <p className="text-sm text-gray-500 mt-2">
           <strong>Instructor:</strong> {course.instructor}
         </p>
-        
+
         <Link
           href="/blog"
           className="mt-5 inline-block px-4 py-2 bg-green-600 text-white rounded hover:bg-blue-700 transition"
@@ -56,8 +51,7 @@ const CourseDetails = () => {
         </Link>
       </div>
       <div className=" md:col-span-2 col-span-7">
-     <h1 className=" text-center text-2xl">You May Also Like</h1>
-     <DetailsLike></DetailsLike>
+        <h1 className=" text-center text-2xl">You May Also Like</h1>
       </div>
 
     </div>
