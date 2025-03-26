078cf4708604e9ff1a949686a5a355fb004d8d52
tariqul420 <tariqul.developer@gmail.com>
2025-03-26 13:08:19 +0600

    Remove redundant class from loading component for cleaner styling

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,7 +2,7 @@ import { Loader } from "lucide-react";
 
 const Loading = () => {
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-100 dark:bg-black dark:bg-black">
+    <div className="flex h-screen items-center justify-center bg-gray-100 dark:bg-black">
       <Loader size={30} className="text-main animate-spin text-4xl" />
     </div>
   );
