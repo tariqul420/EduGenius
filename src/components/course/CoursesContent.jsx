08a70e2d32a728768857f4700f5d2004571022ef
tariqul420 <tariqul.developer@gmail.com>
2025-03-27 11:03:24 +0600

    Add Load More functionality to Blogs and Courses pages: include LoadMore component and pass hasNextPage prop for pagination

--- src/components/course/CoursesContent.jsx
+++ src/components/course/CoursesContent.jsx
@@ -1,17 +1,25 @@
 "use client";
 import useProvider from "@/hooks/useProvider";
 import CourseCard from "../shared/CourseCard";
+import LoadMore from "../shared/LoadMore";
 import EmptyPage from "../shared/NoResult";
 
-export default function CoursesContent({ courses = [] }) {
+export default function CoursesContent({ courses = [], hasNextPage }) {
   const { isGridCol } = useProvider();
 
   return (
     <div
-      className={`courses col-span-12 grid gap-5 lg:col-span-8 ${isGridCol ? "grid-cols-1 gap-5" : "sm:grid-cols-2"}`}
+      className={`courses col-span-12 grid gap-5 lg:col-span-8 ${
+        isGridCol ? "grid-cols-1 gap-5" : "sm:grid-cols-2"
+      }`}
     >
       {courses.length > 0 ? (
-        courses.map((course) => <CourseCard key={course._id} course={course} />)
+        <>
+          {courses.map((course) => (
+            <CourseCard key={course._id} course={course} />
+          ))}
+          {hasNextPage && <LoadMore />}
+        </>
       ) : (
         <div className="col-span-12 grid items-center justify-center">
           <EmptyPage />
