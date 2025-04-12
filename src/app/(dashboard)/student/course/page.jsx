967488a2b43e64a509d83840ecd26d9c2a558eee
tariqul420 <tariqul.developer@gmail.com>
2025-04-12 23:35:40 +0600

    Refactor StudentCertificate and CertificateTable components; update section class for consistency, remove unused imports, and enhance table structure with ShadCN components.

--- src/app/(dashboard)/student/course/page.jsx
+++ src/app/(dashboard)/student/course/page.jsx
@@ -1,9 +1,3 @@
-
-import { StudentsDataTable } from "@/components/StudentsDataTable";
-import data from "@/constant/data.json";
-
 export default function StudentCourse() {
-  return   <section className="py-6">
-        <StudentsDataTable data={data} />
-      </section>;
+  return <section className="py-6"></section>;
 }
