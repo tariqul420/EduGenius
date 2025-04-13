05bc0bfea65174c62f29ccad3d75b950fc5d5119
jahidulkanchan <mdj524156@gmail.com>
2025-04-13 23:51:47 +0600

    style: update "EduGenius" branding in AppSidebar, Footer, and Navbar components for improved visual consistency

--- src/components/dashboard/instructor/AppSidebar.jsx
+++ src/components/dashboard/instructor/AppSidebar.jsx
@@ -59,7 +59,7 @@ export function AppSidebar({ menu = [] }) {
       <SidebarHeader className="bg-light-bg dark:bg-dark-bg">
         <Link href="/" className="flex items-center gap-2 text-3xl">
           <GraduationCap size={26} className="text-main" />
-          <h2 className="text-2xl font-semibold">EduGenius</h2>
+          <h2 className="text-2xl font-semibold">Edu<span className="text-main">Genius</span></h2>
         </Link>
       </SidebarHeader>
       <SidebarContent className="bg-light-bg dark:bg-dark-bg">
