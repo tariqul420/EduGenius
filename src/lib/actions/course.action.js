e23b50a6361fd9a25bca29dbde0022880d3e79e6
tariqul420 <tariqul.developer@gmail.com>
2025-04-20 08:15:05 +0600

    feat: format monetary values to two decimal places and simplify slug matching in course actions

--- src/lib/actions/course.action.js
+++ src/lib/actions/course.action.js
@@ -164,7 +164,7 @@ export async function getCourseBySlug(slug) {
 
     const courses = await Course.aggregate([
       {
-        $match: { slug: slug },
+        $match: { slug },
       },
       {
         $lookup: {
