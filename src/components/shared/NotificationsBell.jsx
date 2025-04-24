5ad5acf693866468ad44bc51f66efed7e7c85cda
tariqul420 <tariqul.developer@gmail.com>
2025-04-24 18:28:45 +0600

    feat: Add tabbed interface for notifications in NotificationsBell component

--- src/components/shared/NotificationsBell.jsx
+++ src/components/shared/NotificationsBell.jsx
@@ -6,6 +6,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function NotificationsBell() {
   return (
@@ -17,7 +18,31 @@ export default function NotificationsBell() {
       <PopoverContent>
         <Alert>
           <AlertDescription>
-            You have no new notifications. Check back later for updates.
+            <Tabs defaultValue="receive" className="w-full">
+              <TabsList className="bg-light-bg dark:bg-dark-hover w-full rounded px-1.5 py-5 shadow-sm">
+                <TabsTrigger
+                  value="receive"
+                  className="data-[state=active]:text-main dark:from-dark-bg dark:to-dark-hover rounded from-white to-white px-6 py-4 text-sm font-medium text-gray-500 transition-all data-[state=active]:bg-gradient-to-b data-[state=active]:shadow-sm dark:data-[state=active]:bg-gradient-to-b dark:data-[state=active]:text-white"
+                >
+                  Receive
+                </TabsTrigger>
+
+                <TabsTrigger
+                  value="send"
+                  className="data-[state=active]:text-main dark:from-dark-bg dark:to-dark-hover rounded from-white to-white px-6 py-4 text-sm font-medium text-gray-500 transition-all data-[state=active]:bg-gradient-to-b data-[state=active]:shadow-sm dark:data-[state=active]:bg-gradient-to-b dark:data-[state=active]:text-white"
+                >
+                  Send
+                </TabsTrigger>
+              </TabsList>
+
+              <TabsContent value="receive" className="mt-6">
+                You have no new notifications. Check back later for updates.
+              </TabsContent>
+
+              <TabsContent value="send" className="mt-6">
+                You have no new notifications. Check back later for updates.
+              </TabsContent>
+            </Tabs>
           </AlertDescription>
         </Alert>
       </PopoverContent>
