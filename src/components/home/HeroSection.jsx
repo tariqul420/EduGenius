669e8117d568b1e3d32c1c5794e742a921ab2d32
jahidulkanchan <mdj524156@gmail.com>
2025-04-26 23:39:17 +0600

    Implement code changes to enhance functionality and improve performance

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -72,7 +72,7 @@ function HeroSection() {
           {/* First Half-width Image */}
           <div className="relative col-span-1 h-[150px] md:h-[200px]">
             <Image
-              src="/hero-image-2.jpg"
+              src="/hero-image-2.png"
               alt="Hero Image"
               fill
               className="rounded-lg object-cover"
